e13d590b9b219575feb8421836883d563de152c0
Awatanabe <panser098@gmail.com>
2019-02-28 08:37:35 +0900

    index authenticate！回避

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
-  describe "#index" do
-    it "responds successfully" do
-      get :index
+ 
+  describe "index" do 
+    it "正常にレスポンスを返すこと" do 
+      get :index 
       expect(response).to be_success
+    end
+
+    it "200レスポンスを返すこと" do 
+      get :index
+      expect(response).to have_http_status "200"
     end
   end
 
-  it "returns a 200 response" do
-    get :index
-    expect(response).to have_http_status "200"
-  end
 end
